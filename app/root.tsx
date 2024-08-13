2f4bbb74506dc2b4cd97f33a223ce1e3305b7f29
jtgi <j@jtgi.me>
2024-08-13 18:22:59 +0900

    favi

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,31 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import "./tailwind.css";
 import "./root.css";
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  {
+    rel: "manifest",
+    href: "/site.webmanifest",
+  },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,31 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { json, type ActionFunctionArgs, type MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
+  return [
+    { title: "Colorful" },
+    { name: "description", content: "Colorful - create shareable code snippets" },
+  ];
 };
 
 export default function Index() {
-  return <div>Code Snippet</div>;
+  return (
+    <div
+      className="flex flex-col gap-4 h-screen w-screen items-center justify-center"
+      style={{
+        animation: "rainbow 0.5s linear infinite",
+      }}
+    >
+      <img src="/apple-touch-icon.png" className="animate-bounce" />
+      <div className="text-center">
+        <p className="mono text-2xl">Colorful</p>
+        <small className="text-sm">
+          make pretty code snippets on farcaster, by{" "}
+          <a href="https://warpcast.com/jtgi" target="_blank" className="underline" rel="noreferrer">
+            @jtgi
+          </a>
+        </small>
+      </div>
+    </div>
+  );
 }
