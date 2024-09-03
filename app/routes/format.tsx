ad23672ac7fde81713fc66c6d06e1fe5464337a8
jtgi <j@jtgi.me>
2024-09-03 12:13:27 +0900

    try n fix dupe embed bug

--- app/routes/format.tsx
+++ app/routes/format.tsx
@@ -12,14 +12,102 @@ import { useLoaderData } from "@remix-run/react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { useDarkMode } from "~/components/dark-mode";
 
-// This should be moved to a separate file, e.g., `app/components/CodeEditor.tsx`
-interface CodeEditorProps {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const rawState = url.searchParams.get("state");
+  const state = JSON.parse(rawState ?? "{}");
+  return { state, hostUrl };
+}
+
+export default function Screen() {
+  const { state, hostUrl: _hostUrl } = useLoaderData<typeof loader>();
+  const { isDarkMode } = useDarkMode();
+
+  const [language, setLanguage] = useState<string | undefined>();
+  const [theme, setTheme] = useState(isDarkMode ? "atom-one-dark" : "atom-one-light");
+  const [height, setHeight] = useState<number | undefined>();
+  const [fontSize, setFontSize] = useState(16.875);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const pattern = /(?:```|:::)([\s\S]*?)(?:```|:::)/;
+  const codeBlockMatch = state.cast.text.match(pattern);
+
+  const initialCode = codeBlockMatch ? toAscii(codeBlockMatch[1].trim()) : "// Go ahead, write some code";
+
+  const handleFinish = async () => {
+    setIsLoading(true);
+    const node = document.querySelector("#codeblock");
+    if (!node) return;
+
+    // bug fix: for react native web view on ios
+    // without calling toCanvas twice it renders blank.
+    // ty: haole#3346
+    await htmlToImage.toCanvas(node as HTMLElement);
+    const canvas = await htmlToImage.toCanvas(node as HTMLElement);
+    const dataUrl = canvas.toDataURL("image/png");
+
+    const response = await fetch("/api/upload-image", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ imageDataUrl: dataUrl }),
+    });
+
+    setIsLoading(false);
+
+    if (!response.ok) throw new Error("Failed to upload image");
+
+    const data = await response.json();
+    const url = `${_hostUrl}${data.path}`;
+    const updatedText = state.cast.text?.replace(pattern, "");
+
+    window.parent.postMessage(
+      {
+        type: "createCast",
+        data: { cast: { text: updatedText, embeds: [url] } },
+      },
+      "*"
+    );
+  };
+
+  return (
+    <div className="p-4 max-w-4xl mx-auto flex flex-col gap-4 mt-8">
+      <CodeEditor
+        initialCode={initialCode}
+        language={language}
+        theme={theme}
+        fontSize={fontSize}
+        height={height}
+      />
+      <CustomizationPanel
+        language={language}
+        setLanguage={setLanguage}
+        theme={theme}
+        setTheme={setTheme}
+        height={height || 0}
+        setHeight={setHeight}
+        fontSize={fontSize}
+        setFontSize={setFontSize}
+      />
+      <button
+        onClick={handleFinish}
+        className={`px-4 py-2 ${isLoading ? "bg-gray-400" : "bg-purple-500"} text-white rounded-md hover:${
+          isLoading ? "" : "bg-purple-600"
+        } transition-colors`}
+        disabled={isLoading}
+      >
+        {isLoading ? "Embedding..." : "Embed"}
+      </button>
+    </div>
+  );
+}
+
+type CodeEditorProps = {
   initialCode: string;
   language?: string;
   theme: string;
   fontSize: number;
   height?: number;
-}
+};
 
 function CodeEditor(props: CodeEditorProps) {
   const { initialCode, language, fontSize, theme, height } = props;
@@ -62,7 +150,7 @@ function CodeEditor(props: CodeEditorProps) {
   );
 }
 
-interface CustomizationPanelProps {
+type CustomizationPanelProps = {
   language: string | undefined;
   setLanguage: (lang: string) => void;
   theme: string;
@@ -71,7 +159,7 @@ interface CustomizationPanelProps {
   setHeight: (height: number) => void;
   fontSize: number;
   setFontSize: (size: number) => void;
-}
+};
 
 function CustomizationPanel(props: CustomizationPanelProps) {
   const { language, setLanguage, theme, setTheme, height, setHeight, fontSize, setFontSize } = props;
@@ -149,102 +237,6 @@ function CustomizationPanel(props: CustomizationPanelProps) {
           </div>
         </div>
       )}
-    </div>
-  );
-}
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
-  const rawState = url.searchParams.get("state");
-  const state = JSON.parse(rawState ?? "{}");
-  return { state, hostUrl };
-}
-
-export default function Screen() {
-  const { state, hostUrl: _hostUrl } = useLoaderData<typeof loader>();
-  const { isDarkMode } = useDarkMode();
-
-  const [language, setLanguage] = useState<string | undefined>();
-  const [theme, setTheme] = useState(isDarkMode ? "atom-one-dark" : "atom-one-light");
-  const [height, setHeight] = useState<number | undefined>();
-  const [fontSize, setFontSize] = useState(16.875);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const pattern = /(?:```|:::)([\s\S]*?)(?:```|:::)/;
-  const codeBlockMatch = state.cast.text.match(pattern);
-
-  const initialCode = codeBlockMatch ? toAscii(codeBlockMatch[1].trim()) : "// Go ahead, write some code";
-
-  const handleFinish = async () => {
-    setIsLoading(true);
-    const node = document.querySelector("#codeblock");
-    if (!node) return;
-
-    // bug fix: for react native web view on ios
-    // without calling toCanvas twice it renders blank.
-    // ty: haole#3346
-    await htmlToImage.toCanvas(node as HTMLElement);
-    const canvas = await htmlToImage.toCanvas(node as HTMLElement);
-    const dataUrl = canvas.toDataURL("image/png");
-
-    const response = await fetch("/api/upload-image", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ imageDataUrl: dataUrl }),
-    });
-
-    setIsLoading(false);
-
-    if (!response.ok) throw new Error("Failed to upload image");
-
-    const data = await response.json();
-    const url = `${_hostUrl}${data.path}`;
-    const updatedText = state.cast.text?.replace(pattern, "");
-
-    const updatedEmbeds = [...(state.cast.embeds || [])];
-    if (updatedEmbeds.length > 1) {
-      updatedEmbeds[1] = url;
-    } else {
-      updatedEmbeds.push(url);
-    }
-
-    window.parent.postMessage(
-      {
-        type: "createCast",
-        data: { cast: { text: updatedText, embeds: updatedEmbeds } },
-      },
-      "*"
-    );
-  };
-
-  return (
-    <div className="p-4 max-w-4xl mx-auto flex flex-col gap-4 mt-8">
-      <CodeEditor
-        initialCode={initialCode}
-        language={language}
-        theme={theme}
-        fontSize={fontSize}
-        height={height}
-      />
-      <CustomizationPanel
-        language={language}
-        setLanguage={setLanguage}
-        theme={theme}
-        setTheme={setTheme}
-        height={height || 0}
-        setHeight={setHeight}
-        fontSize={fontSize}
-        setFontSize={setFontSize}
-      />
-      <button
-        onClick={handleFinish}
-        className={`px-4 py-2 ${isLoading ? "bg-gray-400" : "bg-purple-500"} text-white rounded-md hover:${
-          isLoading ? "" : "bg-purple-600"
-        } transition-colors`}
-        disabled={isLoading}
-      >
-        {isLoading ? "Embedding..." : "Embed"}
-      </button>
     </div>
   );
 }
